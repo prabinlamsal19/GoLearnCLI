41b8f3a24221ac62167e151fc912e846e4889bb3
prabinlamsal19 <lamsalprabin74@gmail.com>
2023-09-13 16:32:46 +0545

    fix: bug fixes and typos

--- main.go
+++ main.go
@@ -1,93 +1,111 @@
-package main 
+package main
 
-import ( 
-	"fmt"
+import (
 	"flag"
+	"fmt"
 	"os"
-)  
+)
 
-func main() { 
-	//'videos get' subcommand 
-	getCmd := flag.NewFlagSet("get", flag.ExitOnError)
+func main() {
+	
+	//'videos get' subcommand
+  getCmd := flag.NewFlagSet("get", flag.ExitOnError)
 
-	// inputs for 'videos get' command 
-	getAll := getCmd.Bool("all" , false , "Get all videos")
-	getID := getCmd.String("id", "", "Youtube video ID")
+	// inputs for `videos get` command
+  getAll := getCmd.Bool("all", false, "Get all videos")
+  getID := getCmd.String("id", "", "YouTube video ID")
 
-	addCmd := flag.NewFlagSet("add", flag.ExitOnError) 
-	addId := addCmd.String("id", "", "Youtube video ID")
-	addTitle := addCmd.String("title", "", "Youtube video Title") 
-	addUrl := addCmd.String("url", "", "YouTube video URL") 
-	addImageUrl := addCmd.String("imageurl", "", "Youtube video Image") 
-	addDesc := addCmd.String("desc", "", "Youtube video description") 
+	//'videos add' subcommand
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 
-	if len(os.Args)< 2 { 
-		fmt.Println("expected 'get' or 'add' subcommands") 
+	// inputs for `videos add` command
+	addID := addCmd.String("id", "", "YouTube video ID")
+  addTitle := addCmd.String("title", "", "YouTube video Title")
+  addUrl := addCmd.String("url", "", "YouTube video URL")
+  addImageUrl := addCmd.String("imageurl", "", "YouTube video Image URL")
+  addDesc := addCmd.String("desc", "", "YouTube video description")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'get' or 'add' subcommands")
 		os.Exit(1)
-	} 
+  }
 
-	//handle get, add and wrong input by the user 
-	switch os.Args[1]{ 
-	case "get": //if its the get command 
-	//handle get here  
-		HandleGet(getCmd, getAll ,getID)
-	case "add": 
-		HandleAdd(addCmd , addID , addTitle , addUrl , addImageUrl , addDesc)
-	default: //if we don't understand the input 
+	//look at the 2nd argument's value
+	switch os.Args[1] {
+		case "get": // if its the 'get' command
+			HandleGet(getCmd, getAll, getID)
+		case "add": // if its the 'add' command
+			HandleAdd(addCmd, addID,addTitle,addUrl, addImageUrl, addDesc)
+		default: // if we don't understand the input
 	}
-}   
 
-func HandleGet(getCmd *flag.FlagSet , all *bool , id *string){ 
-	getCmd.Parse(os.Args[2]) 
 
-	if *all == false && *id == "" { 
-		fmt.Print("id is require or specify --all for all videos")
-		getCmd.PrintDefaults()
-		os.Exit(1)
-	} 
-
-	if *all { 
-		//return all videos
-		videos := getVideos() 
-		fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n") 
-		
-		for _, video := range videos { 
-			fmt.Printf("%v \t %v \t %v \t %v \t %v \n ", video.Id , video.Title ,video.Url , video.ImageUrl , video.Description) 
-		} 
-		return 
-	} 
-
-	if *id != "" { 
-		videos := getVideos() 
-		id := *id 
-		for _, video := range videos { 
-			fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
-			fmt.Printf("%v \t %v \t %v \t %v \t %v \n", video.Id , video.Title , video.Url , video.ImageUrl , video.Description)
-		}
-	}
-}  
-
-func VallidateVideo(addCmd *flag.FlagSet , id *string , title *string , url *string , imageurl *string, description *string) { 
-	if *id == "" || *title == "" || *url == "" || *imageUrl == "" || *description = "" { 
-		fmt.Print("all fields are required for adding a video") 
-		addCmd.PrintDefaults() //built in in the flag package
-		os.Exit(1) 
-	}
 }
 
-func HandleAdd(addCmd *flag.FlagSet , id *string , title *string , url *string , imageurl *string, description *string) { 
-	VallidateVideo(addCmd , id , title, url , imageUrl ,description) 
+func HandleGet(getCmd *flag.FlagSet, all *bool, id *string){
 
-	video := video { 
-		Id : *id , 
-		Title : *title, 
-		Description: *description, 
-		Imageurl : *imageUrl, 
+	getCmd.Parse(os.Args[2:])
+
+	if *all == false && *id == "" {
+    fmt.Print("id is required or specify --all for all videos")
+    getCmd.PrintDefaults()
+    os.Exit(1)
+  }
+
+	if *all {
+		//return all videos
+		videos := getVideos()
+		
+		fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
+		for _, video := range videos {
+			fmt.Printf("%v \t %v \t %v \t %v \t %v \n",video.Id, video.Title, video.Url, video.Imageurl,video.Description)
+		}
+
+		return
+	}
+
+	if *id != "" {
+		videos := getVideos()
+		id := *id
+		for _, video := range videos {
+			if id == video.Id {
+				fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
+				fmt.Printf("%v \t %v \t %v \t %v \t %v \n",video.Id, video.Title, video.Url, video.Imageurl,video.Description)
+			}
+		}
+	} 
+
+
+
+}
+
+func ValidateVideo(addCmd *flag.FlagSet,id *string, title *string, url *string, imageUrl *string, description *string ){
+
+	addCmd.Parse(os.Args[2:])
+
+	if *id == "" || *title == "" || *url == "" || *imageUrl == "" || *description == "" {
+		fmt.Print("all fields are required for adding a video")
+		addCmd.PrintDefaults()
+		os.Exit(1)
+	}
+
+}
+
+func HandleAdd(addCmd *flag.FlagSet,id *string, title *string, url *string, imageUrl *string, description *string ){
+
+	ValidateVideo(addCmd, id,title,url, imageUrl, description)
+
+	video := video{
+		Id: *id,
+		Title: *title,
+		Description: *description,
+		Imageurl: *imageUrl, 
 		Url: *url, 
-	}  
+	}
 
-	videos := getVideos() 
-	videos = append(videos,video) 
+	videos := getVideos()
+	videos = append(videos,video)
 
-	saveVideos(videos) 
-} 
+	saveVideos(videos)
+
+}
--- videos.go
+++ videos.go
@@ -1,41 +1,45 @@
-package main 
+package main
 
-import { 
-	"io/ioutil" 
+import (
+	"io/ioutil"
 	"encoding/json"
-}
+)
 
-type video struct { 
-	Id string 
-	Title string 
+type video struct {
+	Id string  
+	Title string  
 	Description string 
 	Imageurl string 
 	Url string 
-} 
+}
 
-func getVideos()(videos []video){  
-	fileBytes, err := ioutil.ReadFile("./videos.json") 
+func getVideos()(videos []video){
+	
+	fileBytes, err := ioutil.ReadFile("./videos.json")
 
-	if err != nil{ 
-		panic(err)
-	}  
-	var video videos
-	err = json.Unmarshal(fileBytes, &videos) 
-	//to convert the bytearray to unmarshall slice(videos) 
-
-	if err != nil { 
+	if err != nil {
 		panic(err)
 	}
+
+	err = json.Unmarshal(fileBytes, &videos)
+
+	if err != nil {
+		panic(err)
+	}
+
 	return videos
 }
 
-func saveVideos(videos []video) () { 
-	videoBytes , err := json.Marshal(videos) 
-	if err != nil{ 
-		panic(err)
-	} 
-	err = ioutil.WriteFile("./videos.json", videoBytes, 0644)
-	if err != nil{ 
+func saveVideos(videos []video)(){
+
+  videoBytes, err  := json.Marshal(videos)
+  if err != nil {
 		panic(err)
 	}
+
+	err = ioutil.WriteFile("./videos.json", videoBytes, 0644)
+  if err != nil {
+		panic(err)
+	}
+
 }
